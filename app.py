309035bb0f98fbbc10c3516106949cd711990960
Harshit Kumar <ts0445852@gmail.com>
2026-07-18 23:18:52 +0530

    fix deploy error

--- app.py
+++ app.py
@@ -74,5 +74,8 @@ def health():
     return jsonify({"status":"ok","model":META["model_name"],
                     "accuracy":META["accuracy"],"f1":META["f1_score"]})
 
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=5000)
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
